3a111153d466198d10f6d4107bbfde50a01bc230
khs <hyeongs2323@gmail.com>
2023-04-08 18:17:05 +0900

    feat: drag x & y & true setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
+
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -18,36 +19,17 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 const boxVariants = {
   hover: { rotateZ: 90 },
   click: { borderRadius: "100px" },
 };
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+      {/* x & y & true drag setting */}
+      <Box style={{ x }} drag={true} dragSnapToOrigin />
     </Wrapper>
   );
 }
