9a1027d1284a7a87fcb33051a7f469c9ecf8abf8
khs <hyeongs2323@gmail.com>
2023-04-08 18:31:45 +0900

    feat: box drag x + rotate & background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
-const Wrapper = styled.div`
-  height: 100vh;
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
+
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -24,11 +30,20 @@ const boxVariants = {
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
-      {/* x & y & true drag setting */}{" "}
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
